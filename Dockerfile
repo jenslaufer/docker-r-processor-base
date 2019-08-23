372821235b22df4a05a00c9f3a0feacfacabfa44
Jens Laufer <jenslaufer@gmail.com>
2019-08-23 09:15:36 +0200

    initial

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+from rocker/tidyverse:3.6.1
+
+ADD requirements.R requirements.R
+
+RUN Rscript requirements.R
---
+++ requirements.R
@@ -0,0 +1,7 @@
+update.packages(lib.loc="/usr/local/lib/R/site-library", ask=FALSE, checkBuilt=TRUE, repos = "https://cloud.r-project.org")
+
+list.of.packages <- c("argparse", "glue", "mosaic", "logging")
+new.packages <-
+  list.of.packages[!(list.of.packages %in% installed.packages()[, "Package"])]
+if (length(new.packages))
+  install.packages(new.packages)
