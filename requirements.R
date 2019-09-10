53f727a25c6453d75ee6fc22658f6be27cabc569
Jens Laufer <jenslaufer@gmail.com>
2019-09-10 14:42:09 +0200

    added mongolite + plumber

--- requirements.R
+++ requirements.R
@@ -1,6 +1,6 @@
 update.packages(lib.loc="/usr/local/lib/R/site-library", ask=FALSE, checkBuilt=TRUE, repos = "https://cloud.r-project.org")
 
-list.of.packages <- c("argparse", "glue", "mosaic", "logging")
+list.of.packages <- c("argparse", "glue", "mosaic", "logging", "mongolite", "plumber")
 new.packages <-
   list.of.packages[!(list.of.packages %in% installed.packages()[, "Package"])]
 if (length(new.packages))
